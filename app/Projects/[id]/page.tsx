966a0dc0c4f212d0f37c9299f8b84f583024ebc4
Markwitz Clement <clem.markwitz@gmail.com>
2025-04-27 15:48:36 +0200

    ajout bouton retour

--- app/Projects/[id]/page.tsx
+++ app/Projects/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';  // Indiquer que ce fichier est un Client Component
 
-import { useParams } from 'next/navigation';  // Importation correcte de useParams
+import { useParams,useRouter } from 'next/navigation';  // Importation correcte de useParams
 import { SideProjectList } from "@/app/_components/Status";
 import { Header } from "../../_components/Header";
 import { Spacing } from "../../_components/Spacing";
@@ -8,6 +8,7 @@ import { Footer } from '@/app/_components/Footer';
 
 export default function ProjectDetails() {
   const { id } = useParams();  // Utilisation de useParams pour obtenir l'ID
+  const router = useRouter(); 
 
   if (!id) {
     return <h1>Projet non trouvé</h1>;
@@ -31,6 +32,12 @@ export default function ProjectDetails() {
     <section className="  dark:bg-gray-900">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
+        <button
+              onClick={() => router.push('/')}
+              className="mb-6 inline-flex items-center px-5 py-3 text-base font-medium text-center text-muted-foreground border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              ← Retour à l'accueil
+            </button>
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{project.title}</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{project.description}</p>
             
